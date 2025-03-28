dc493d8beaaa0edcf4912b7e96f203ff0a468f70
Evan Yan <103996156+evanyan13@users.noreply.github.com>
2025-03-28 15:19:03 -0700

    [CI-4283] Hooks for useCioClient (#5)
    * Create mock useCioClient hook
    
    * Adding tests
    
    * Update file naming convention
    
    * Update file naming
    
    * Standardize syntax for test files
    
    * Create assistant mock class and method
    
    * Update storybook example
    
    * Update implementation for mock client
    
    * Add options to mock cnstrc client object
    
    * Update implementation of mock client
    
    * Update apiKey and method response structure
    
    * Update getSuggestedQuestions response structure
    
    * Rename story
    
    * Update fetch for test
    
    * Add mock fetch to test
    
    ---------
    
    Co-authored-by: Evan Yan <evan.yan@constructor.io>

---
+++ src/hooks/mocks/assistant.ts
@@ -0,0 +1,61 @@
+import { ConstructorClientOptions } from '@constructor-io/constructorio-client-javascript';
+
+interface QuestionResponse {
+  questions: Array<string>;
+}
+
+// Create URL from supplied intent (term) and parameters
+function createAssistantUrl(
+  itemId: string,
+  endpoint: string,
+  options: ConstructorClientOptions,
+  parameters: Record<string, string> = {},
+) {
+  const { apiKey, assistantServiceUrl } = options;
+  const baseUrl = `${assistantServiceUrl}/v1/${endpoint}`;
+
+  const url = new URL(baseUrl);
+  url.searchParams.append('item_id', itemId);
+  url.searchParams.append('key', apiKey);
+
+  // Any additional parameters
+  Object.entries(parameters).forEach(([key, value]) => {
+    if (value !== undefined) {
+      url.searchParams.append(key, String(value));
+    }
+  });
+
+  return url.toString();
+}
+
+class MockAssistant {
+  options: ConstructorClientOptions;
+
+  constructor(options: ConstructorClientOptions) {
+    this.options = options;
+  }
+
+  async getSuggestedQuestions(itemId: string, parameters: Record<string, any> = {}): Promise<QuestionResponse> {
+    if (!this.options.apiKey) {
+      throw new Error('API key is required');
+    }
+
+    const url = createAssistantUrl(itemId, 'item_questions', this.options, parameters);
+
+    try {
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error(`Get Suggested Questions failed with status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      const errorMessage = error instanceof Error ? error.message : String(error);
+      throw new Error(`Get Suggested Questions failed: ${errorMessage}`);
+    }
+  }
+}
+
+export default MockAssistant;
---
+++ src/hooks/useCioClient.ts
@@ -0,0 +1,43 @@
+import { useMemo } from 'react';
+import {
+  ConstructorClientOptions,
+  Nullable,
+} from '@constructor-io/constructorio-client-javascript';
+import version from '../version';
+import MockConstructorIOClient from './mocks/MockConstructorIOClient';
+
+/**
+ * Uses MockConstructorIOClient for now, to be replaced with the actual Client JS object
+ */
+export type UseCioClientProps = {
+  apiKey?: string;
+  cioClient?: Nullable<MockConstructorIOClient>;
+  options?: Omit<ConstructorClientOptions, 'apiKey' | 'sendTrackingEvents' | 'version'>;
+};
+
+type UseCioClient = (props: UseCioClientProps) => Nullable<MockConstructorIOClient> | never;
+
+const useCioClient: UseCioClient = ({ apiKey, cioClient, options } = {}) => {
+  if (!apiKey && !cioClient) {
+    throw new Error('Api Key or Constructor Client required');
+  }
+
+  const memoizedCioClient = useMemo(() => {
+    if (cioClient) return cioClient;
+
+    if (apiKey) {
+      return new MockConstructorIOClient({
+        apiKey,
+        sendTrackingEvents: true,
+        version: `cio-ui-asa-pdp-${version}`,
+        ...options,
+      });
+    }
+
+    return null;
+  }, [apiKey, cioClient, options]);
+
+  return memoizedCioClient!;
+};
+
+export default useCioClient;
