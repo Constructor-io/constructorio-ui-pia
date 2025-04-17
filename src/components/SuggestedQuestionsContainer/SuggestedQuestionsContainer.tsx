46e174ef4dffde55a4aac50ed2b329f3f1efc94d
Evan Yan <103996156+evanyan13@users.noreply.github.com>
2025-04-17 14:27:13 -0700

    CI-4274 Add Suggested Questions Layout (#8)
    * Add suggested questions container
    
    * Add styles
    
    * Add test cases
    
    * Add error handling
    
    * Add error component
    
    * Update component names
    
    * Remove error component
    
    * Update css
    
    * Update component names in test
    
    * Update tests
    
    * Update useSuggestQuestions tests
    
    * Update test cases
    
    * Update test message
    
    * Migrate component to separate folder
    
    * Update responsiveness handling
    
    * Add loading state from hook
    
    * Update css
    
    * Move tests
    
    * Lint
    
    * Lint
    
    ---------
    
    Co-authored-by: Evan Yan <evan.yan@constructor.io>

---
+++ src/components/SuggestedQuestionsContainer/SuggestedQuestionsContainer.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import SuggestedQuestion from '../SuggestedQuestion/SuggestedQuestion';
+import useSuggestedQuestions from '../../hooks/useSuggestedQuestions';
+import { Question } from '../../hooks/mocks/assistant';
+
+interface SuggestedQuestionsContainerProps {
+  itemId: string;
+  onQuestionClick: (question: Question) => void;
+  isError?: boolean;
+}
+
+export default function SuggestedQuestionsContainer({
+  itemId,
+  onQuestionClick,
+  isError = false,
+}: SuggestedQuestionsContainerProps) {
+  // Todo: Replace with useSuggestedQuestions hook
+  const { questions, error } = useSuggestedQuestions({ itemId });
+
+  if (!questions || questions.length === 0) {
+    return null;
+  }
+
+  if (isError || error) {
+    const errorMessage = error?.message ?? 'Error fetching suggested questions';
+    return (
+      <div data-testid='suggested-questions-container-error-block'>
+        To be replaced with ErrorBlock component: {errorMessage}
+      </div>
+    );
+  }
+
+  return (
+    <div
+      className='cio-asa-pdp-suggested-questions-container'
+      data-testid='suggested-questions-list'>
+      {questions.map((question) => (
+        <SuggestedQuestion
+          key={question.value}
+          question={question.value}
+          onClick={() => onQuestionClick(question)}
+        />
+      ))}
+    </div>
+  );
+}
