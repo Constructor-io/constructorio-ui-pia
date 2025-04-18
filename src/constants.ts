f5ee2c82857288c64b0218fd692773ea0f31839d
Evan Yan <103996156+evanyan13@users.noreply.github.com>
2025-04-18 08:31:37 -0700

    CI-4353 Create getAnswerResults function in mock js client (#13)
    * Add getAnswerResults and getAnswerResultsSream to mock client
    
    * Update url creation
    
    ---------
    
    Co-authored-by: Evan Yan <evan.yan@constructor.io>

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,7 @@
 // eslint-disable-next-line import/prefer-default-export
 export const DEMO_API_KEY = '***REMOVED***';
 export const DEMO_ITEM_ID = '22368';
+export const DEMO_QUESTION = 'What are the dimensions of the swim cap?';
 export const MOCK_QUESTIONS = [
   { value: 'Is this bunkie board suitable for a platform bed?' },
   { value: 'What are the benefits of using a bunkie board with a platform bed?' },
