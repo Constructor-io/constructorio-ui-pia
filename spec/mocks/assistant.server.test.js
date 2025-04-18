4f1e8c931f32c9d9177c36e1868d881a7ce8d476
Evan Yan <103996156+evanyan13@users.noreply.github.com>
2025-04-18 08:31:37 -0700

    CI-4353 Create getAnswerResults function in mock js client (#13)
    * Add getAnswerResults and getAnswerResultsSream to mock client
    
    * Update url creation
    
    ---------
    
    Co-authored-by: Evan Yan <evan.yan@constructor.io>

--- spec/mocks/assistant.server.test.js
+++ spec/mocks/assistant.server.test.js
@@ -1,25 +1,45 @@
 import MockConstructorIOClient from '../../src/hooks/mocks/MockConstructorIOClient';
-import { DEMO_API_KEY } from '../../src/constants';
+import { DEMO_API_KEY, DEMO_ITEM_ID, DEMO_QUESTION } from '../../src/constants';
 
 describe('Testing Mocks: Assistant', () => {
-    let client;
+  let client;
 
-    beforeEach(() => {
-        client = new MockConstructorIOClient({
-            apiKey: DEMO_API_KEY,
-            sessionId: 123,
-            clientId: 'test-client-id',
-        })
-    })
+  beforeEach(() => {
+    client = new MockConstructorIOClient({
+      apiKey: DEMO_API_KEY,
+      sessionId: 123,
+      clientId: 'test-client-id',
+    });
+  });
 
-    describe('getSuggestedQuestions', () => {
-        it('Should fetch suggested questions given item_id', async () => {
-            const itemId = '22368';
-            const result = await client.assistant.getSuggestedQuestions(itemId);
-            
-            expect(result).toBeDefined();
-            expect(result.questions).toBeDefined();
-            expect(Array.isArray(result.questions)).toBe(true);
-          });
-    })
-})
+  describe('getSuggestedQuestions', () => {
+    it('Should fetch suggested questions given item_id', async () => {
+      const result = await client.assistant.getSuggestedQuestions(DEMO_ITEM_ID);
+
+      expect(result).toBeDefined();
+      expect(result.questions).toBeDefined();
+      expect(Array.isArray(result.questions)).toBe(true);
+      expect(result.questions[0]).toHaveProperty('value');
+      expect(typeof result.questions[0].value).toBe('string');
+    });
+  });
+
+  describe('getAnswerResults', () => {
+    it('Should fetch answer given item_id and questions', async () => {
+      const result = await client.assistant.getAnswerResults({
+        itemId: DEMO_ITEM_ID,
+        question: DEMO_QUESTION,
+      });
+
+      expect(result).toBeDefined();
+      expect(result).toHaveProperty('value');
+      expect(result.value).toBeDefined();
+      expect(typeof result.value).toBe('string');
+      expect(result.alternatives).toBeUndefined();
+      expect(result.follow_up_questions).toBeDefined();
+      expect(Array.isArray(result.follow_up_questions)).toBe(true);
+      expect(result.follow_up_questions[0]).toHaveProperty('value');
+      expect(typeof result.follow_up_questions[0].value).toBe('string');
+    });
+  });
+});
---
+++ src/hooks/mocks/types.ts
@@ -0,0 +1,48 @@
+import { ConstructorClientOptions } from '@constructor-io/constructorio-client-javascript';
+
+export type AssistantUrlProps = {
+  itemId: string;
+  question?: string;
+  isStreaming?: boolean;
+  options: ConstructorClientOptions;
+  parameters?: Record<string, string>;
+};
+
+export type GetAnswerResultsProps = {
+  itemId: string;
+  question: string;
+  parameters?: Record<string, any>;
+};
+
+export type GetAnswerResultsStreamProps = GetAnswerResultsProps & {
+  onStart?: (event: StreamStartEvent) => void;
+  onMessage?: (event: StreamMessageEvent) => void;
+  onEnd?: (event: StreamEndEvent) => void;
+};
+
+export interface Question {
+  value: string;
+}
+
+export interface QuestionResponse {
+  questions: Array<Question>;
+}
+
+export interface AnswerResponse {
+  value: string;
+  alternative: string;
+  follow_up_questions: Array<string>;
+}
+
+export interface StreamStartEvent {
+  qna_result_id: string;
+}
+
+export interface StreamMessageEvent {
+  qna_result_id: string;
+  text: string;
+}
+
+export interface StreamEndEvent {
+  qna_result_id: string;
+}
