79a8ad031306b9dbc3d73e6a6cf6390c32a6817a
Evan Yan <evan.yan@constructor.io>
2025-04-23 08:47:40 -0700

    Migrate streaming event const

--- src/hooks/mocks/constant.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line prettier/prettier, import/prefer-default-export
-export const STREAM_EVENTS = {
-  START: 'start',
-  MESSAGE: 'message',
-  FOLLOW_UP_QUESTIONS: 'follow_up_questions',
-  END: 'end',
-  ABORT: 'abort',
-};
