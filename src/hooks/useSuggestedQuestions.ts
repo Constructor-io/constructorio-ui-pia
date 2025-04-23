d3e7bd79d9bfc2e4a82041a5fa9db8a8f8af2b47
Evan Yan <evan.yan@constructor.io>
2025-04-23 08:36:41 -0700

    Lint

--- src/hooks/useSuggestedQuestions.ts
+++ src/hooks/useSuggestedQuestions.ts
@@ -6,6 +6,7 @@ import MockConstructorIOClient from './mocks/MockConstructorIOClient';
 
 export interface UseSuggestedQuestionsProps {
   itemId: string;
+  parameter?: Record<string, any>;
 }
 
 interface UseSuggestedQuestionsResponse {
@@ -20,10 +21,9 @@ const fetchSuggestedQuestions = async (client: MockConstructorIOClient, itemId: 
   return response.questions;
 };
 
-export default function useSuggestedQuestions(
-  props: UseSuggestedQuestionsProps,
-): UseSuggestedQuestionsResponse {
-  const { itemId } = props;
+export default function useSuggestedQuestions({
+  itemId,
+}: UseSuggestedQuestionsProps): UseSuggestedQuestionsResponse {
   const client = useCioClient({ apiKey: DEMO_API_KEY });
   const [questions, setQuestions] = useState<Array<Question>>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
