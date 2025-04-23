1b972271f1e3ad6bba2096c0f1e7efcda59a9db7
Evan Yan <103996156+evanyan13@users.noreply.github.com>
2025-04-23 08:49:32 -0700

    NOCI - Fix lint issues in config files (#14)
    * Fix linting issues in config files
    
    * Update variable naming
    
    * Lint
    
    ---------
    
    Co-authored-by: Evan Yan <evan.yan@constructor.io>

--- src/constants.ts
+++ src/constants.ts
@@ -3,10 +3,10 @@ export const DEMO_API_KEY = '***REMOVED***';
 export const DEMO_ITEM_ID = '22368';
 export const DEMO_QUESTION = 'What are the dimensions of the swim cap?';
 export const MOCK_QUESTIONS = [
-  { value: 'Is this bunkie board suitable for a platform bed?' },
-  { value: 'What are the benefits of using a bunkie board with a platform bed?' },
-  { value: 'What is a bunkie board and how does it differ from a box spring?' },
-  { value: 'Is this bunkie board made in the USA?' },
-  { value: 'What sizes are available for this bunkie board?' },
-  { value: 'Can this bunkie board be used with a memory foam mattress?' },
+  { value: 'Is this bunk board suitable for a platform bed?' },
+  { value: 'What are the benefits of using a bunk board with a platform bed?' },
+  { value: 'What is a bunk board and how does it differ from a box spring?' },
+  { value: 'Is this bunk board made in the USA?' },
+  { value: 'What sizes are available for this bunk board?' },
+  { value: 'Can this bunk board be used with a memory foam mattress?' },
 ];
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
-import { defineConfig } from "vite";
-import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+import { defineConfig } from 'vite';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 export default defineConfig({
-  plugins: [cssInjectedByJsPlugin({
-    styleId: 'cio-asa-pdp-styles'
-  })],
+  plugins: [
+    cssInjectedByJsPlugin({
+      styleId: 'cio-asa-pdp-styles',
+    }),
+  ],
   build: {
     rollupOptions: {
       preserveEntrySignatures: 'strict',
       input: {
-        app: "./src/bundled.jsx",
+        app: './src/bundled.jsx',
       },
       output: {
         entryFileNames: `constructorio-ui-asa-pdp-bundled.js`,
