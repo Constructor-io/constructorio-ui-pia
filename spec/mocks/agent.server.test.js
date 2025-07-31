2a495602d09b8702deed813b485975a1c7d0ee88
Evan Yan <103996156+evanyan13@users.noreply.github.com>
2025-07-31 16:10:19 -0700

    [CI-4674] Update serviceUrl in mock client js (#32)
    * Update serviceUrl to use agent
    
    * Update client js version
    
    * Add guard clause to createAgentUrl
    
    * Update package.json
    
    ---------
    
    Co-authored-by: Evan Yan <evan.yan@constructor.io>

--- spec/mocks/agent.server.test.js
+++ spec/mocks/agent.server.test.js
@@ -22,6 +22,31 @@ describe('Testing Mocks: Agent', () => {
       expect(result.questions[0]).toHaveProperty('value');
       expect(typeof result.questions[0].value).toBe('string');
     });
+
+    it('throws an error if no agentServiceUrl is provided', async () => {
+      const clientWithoutUrl = new MockConstructorIOClient({
+        apiKey: DEMO_API_KEY,
+        sessionId: 123,
+        clientId: 'test-client-id',
+        agentServiceUrl: undefined, // Simulating no URL provided
+      });
+
+      await expect(clientWithoutUrl.agent.getSuggestedQuestions(DEMO_ITEM_ID)).rejects.toThrow(
+        'Agent service URL is required',
+      );
+    });
+
+    it('throws an error if no item id is provided', async () => {
+      const clientWithoutUrl = new MockConstructorIOClient({
+        apiKey: DEMO_API_KEY,
+        sessionId: 123,
+        clientId: 'test-client-id',
+      });
+
+      await expect(clientWithoutUrl.agent.getSuggestedQuestions(undefined)).rejects.toThrow(
+        'Item ID is required',
+      );
+    });
   });
 
   describe('getAnswerResults', () => {
@@ -41,5 +66,51 @@ describe('Testing Mocks: Agent', () => {
       expect(result.follow_up_questions[0]).toHaveProperty('value');
       expect(typeof result.follow_up_questions[0].value).toBe('string');
     }, 15000); // Adding a timeout here as the answer API might take longer to respond
+
+    it('throws an error if no agentServiceUrl is provided', async () => {
+      const clientWithoutUrl = new MockConstructorIOClient({
+        apiKey: DEMO_API_KEY,
+        sessionId: 123,
+        clientId: 'test-client-id',
+        agentServiceUrl: undefined, // Simulating no URL provided
+      });
+
+      await expect(
+        clientWithoutUrl.agent.getAnswerResults({
+          itemId: DEMO_ITEM_ID,
+          question: DEMO_QUESTION,
+        }),
+      ).rejects.toThrow('Agent service URL is required');
+    });
+
+    it('throws an error if no item id is provided', async () => {
+      const clientWithoutUrl = new MockConstructorIOClient({
+        apiKey: DEMO_API_KEY,
+        sessionId: 123,
+        clientId: 'test-client-id',
+      });
+
+      await expect(
+        clientWithoutUrl.agent.getAnswerResults({
+          itemId: undefined,
+          question: DEMO_QUESTION,
+        }),
+      ).rejects.toThrow('Item ID is required');
+    });
+
+    it('throws an error if no question is provided', async () => {
+      const clientWithoutUrl = new MockConstructorIOClient({
+        apiKey: DEMO_API_KEY,
+        sessionId: 123,
+        clientId: 'test-client-id',
+      });
+
+      await expect(
+        clientWithoutUrl.agent.getAnswerResults({
+          itemId: DEMO_ITEM_ID,
+          question: undefined,
+        }),
+      ).rejects.toThrow('Question is required');
+    });
   });
 });
