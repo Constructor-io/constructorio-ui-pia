29b0f43d87af4f7d3a03f287d749cbe31f05ecd2
Vlad Saitov <vladislav.saitov@constructor.io>
2026-04-29 19:35:46 +0500

    Remove unnecessary dep in useEffect

--- src/hooks/useAnswerResults.ts
+++ src/hooks/useAnswerResults.ts
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useRef, useState } from 'react';
 import { Nullable } from '@constructor-io/constructorio-client-javascript';
 import MockConstructorIOClient from './mocks/MockConstructorIOClient';
 import { Item, GetAnswerResultsResponse } from '../types';
@@ -77,6 +77,9 @@ export default function useAnswerResults({
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<Error | null>(null);
 
+  const formatImageUrlRef = useRef(formatImageUrl);
+  formatImageUrlRef.current = formatImageUrl;
+
   const fetchResult = useCallback(
     (question: string) => {
       if (!cioClient) return;
@@ -87,7 +90,7 @@ export default function useAnswerResults({
       fetchAnswerResults({ client: cioClient, itemId, question, variationId, threadId })
         .then((fetchedAnswerResults) => {
           setAnswerResults(fetchedAnswerResults);
-          setItems(extractAndTransformItems(fetchedAnswerResults, formatImageUrl));
+          setItems(extractAndTransformItems(fetchedAnswerResults, formatImageUrlRef.current));
           setError(null);
         })
         .catch((err) => {
@@ -99,7 +102,7 @@ export default function useAnswerResults({
           setIsLoading(false);
         });
     },
-    [cioClient, itemId, variationId, threadId, formatImageUrl],
+    [cioClient, itemId, variationId, threadId],
   );
 
   return {
