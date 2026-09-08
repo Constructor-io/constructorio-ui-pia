764ee87aab02771833e57f744bcabcfa6b0cde9f
Vladislav Saitov <96235348+vladconstructor@users.noreply.github.com>
2026-09-08 16:30:20 +0500

    [NO-REF] - introduce react versions test workflow (#145)
    * [NO-REF] - introduce react versions test workflow
    
    * Exclude react-compat tests from general jest config
    
    * Ensure compat workflow is run before publish
    
    * Replace ci with simple install
    
    * Remove lock files
    
    * Fix comments
    
    * Unite 16 and 17 into a single entry, and future versions for different one
    
    * Address comments
    
    * Add typecheck
    
    * Add dependency matcher

---
+++ test/react-compat/setup.ts
@@ -0,0 +1,13 @@
+class IntersectionObserverMock {
+  readonly root: Element | null = null;
+  readonly rootMargin: string = '';
+  readonly thresholds: ReadonlyArray<number> = [];
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+  takeRecords(): IntersectionObserverEntry[] {
+    return [];
+  }
+}
+
+globalThis.IntersectionObserver = IntersectionObserverMock as unknown as typeof IntersectionObserver;
